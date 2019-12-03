44f3712fd9058604f993c62face57d7c6791760f
zieglebt <zieglebt@LCBLAB2110-29.uccob.uc.edu>
2019-12-03 09:14:00 -0500

    end of tuesday

--- StringStuff/src/strings/Main.java
+++ StringStuff/src/strings/Main.java
@@ -1,6 +1,6 @@
 /*
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * Brett Ziegler
+ * zieglebt@mail.uc.edu
  * Demonstrating the String and StringBuilder data types
  */
 package strings;
@@ -13,8 +13,13 @@ public class Main {
 	public static void testSomeStrings() {
 		String test = "Todd Lindemann";
 		long startTime = System.nanoTime();
-		for (int i = 0; i < 1000000000; i++) {
-
+		for (int i = 0; i < 1000000000; i++) {	// One billion iterations
+//			test = "Oscar Robertson";	// Init a string variable with a string literal
+//			test = test + "!";	// Append operation with a String type. This creates a NEW string
+			
+			String fruit = "Applee";	// Oops. spelled wrong
+			fruit = fruit.replace("ee","e");
+			//System.out.println(fruit);
 		}
 		long endTime = System.nanoTime();
 
@@ -29,7 +34,17 @@ public class Main {
 		StringBuilder test = new StringBuilder("Todd Lindemann");
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
-
+			//test = new StringBuilder("Oscar Robertson");	// Essentially the same as above
+			//test = test + "!";
+			// How do I append to a StringBuilder object?
+			//test.append("!");	//Yay!
+			
+			//						  0123456
+			test = new StringBuilder("Applee");
+//			System.out.println("Before replace, test = " + test.toString());
+			test.replace(4,7,"e");	// replace ee with e
+//			System.out.println("After replace, test = " + test.toString());
+//			System.out.println("");
 		}
 		long endTime = System.nanoTime();
 
